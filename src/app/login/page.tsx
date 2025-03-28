98e7838045c20cf34dad59f4b30a212390d45cc3
ricardoAlmeidaEtic <ricardo_almeida@eticalgarve.com>
2025-03-28 19:55:47 +0000

    [patch] removed missing icon of google.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -37,11 +37,6 @@ function LoginPageContent() {
           onClick={() => signIn('google')}
           className="w-full flex items-center justify-center gap-2 bg-green-500 text-white px-4 py-3 rounded-lg hover:bg-green-600 transition-all"
         >
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png"
-            alt="Google Logo"
-            className="w-5 h-5"
-          />
           Sign in with Google
         </button>
         <p className="text-gray-400 text-sm text-center mt-6">
